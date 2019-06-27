4e34fc74d4c94f1dde895837ce7c0e6547cf2627
Lightsnake <andrew.tsan@yahoo.com>
2019-06-27 18:40:32 -0400

    initial committ

---
+++ handDetection/app.js
@@ -0,0 +1,29 @@
+navigator.getUserMedia = 
+    navigator.getUserMedia||
+    navigator.webkitGetUserMedia||
+    navigator.mozGetUserMedia||
+    navigator.msGetUserMedia;
+
+//Select everything in the html
+
+const video = document.getElementById('video');
+const audio = document.getElementById('audio');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+let model;
+
+handTrack.startVideo(video)
+    .then(status=>{
+        if( status ){
+            navigator.getUserMedia({video: {}}, stream => {
+                video.srcObject = stream;
+            },
+            err =>console.log(err) 
+        );
+        }
+    })
+
+handTrack.load()
+    .then(lmodel=>{
+        model = lmodel
+    });
